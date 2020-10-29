b70d2492c52dde5d7992f4d1ea1916caabdec6d7
Ilya Piatrenka <ilya.piatrenka@daftcode.pl>
2020-10-29 10:04:35 +0100

    Add SPM support

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ChatProvidersSDK",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "ChatProvidersSDK",
+            targets: ["ChatProvidersSDK"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .binaryTarget(
+            name: "ChatProvidersSDK",
+            path: "ChatProvidersSDK.xcframework"),
+    ]
+)
