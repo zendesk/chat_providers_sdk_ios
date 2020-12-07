56ed16293aa3807c12492ac8f403084cee67fc2c
Alan O'Connor <aloconnor@zendesk.com>
2020-12-07 12:57:57 +0000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,8 +14,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ZendeskChatProvidersSDK",
-            url: "https://github.com/zendesk/chat_providers_sdk_ios/releases/download/2.10.0/ChatProvidersSDK.xcframework.zip",
-            checksum: "4d2b4dd43d1b98d0d405809138a2a2cdec0f4fbca46677d53cb896f52e7ff33f"
+            path: "ChatProvidersSDK.xcframework"
         )
     ]
 )
