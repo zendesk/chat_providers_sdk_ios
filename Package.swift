60db4a62dd2ec0c4cbc213b869c7df199b910ab1
runner <runner@Mac-1666101150918.local>
2022-10-18 14:06:24 +0000

    release/3.0.0

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "ZendeskChatProvidersSDK",
     platforms: [
-        .iOS(.v10)
+        .iOS(.v11)
     ],
     products: [
         .library(
